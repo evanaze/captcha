ba0157a0625f1a1037908b24d4b56328f3159fbf
evanaze <evan@azevedo.com>
2020-05-02 15:58:17 -0700

    fixed synthetic

--- src/synthetic.py
+++ src/synthetic.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 import cv2 as cv 
 import matplotlib.pyplot as plt
-import torchvision.transforms.functional as TF
+from skimage.transform import rotate
+from skimage.io import imsave
 from torchvision import transforms
 from . import config
 
@@ -12,17 +13,20 @@ def make_synthetic(image, f_out):
     laplacian = cv.Laplacian(image, cv.CV_64F)
     squares_channel = cv.split(laplacian)[0].astype(np.uint8)
     thresh = cv.threshold(squares_channel, 60, 255, cv.THRESH_BINARY)[1]
-    thresh = TF.to_pil_image(thresh)
     f_split = f_out.split("."); f_split[1] = ".png"
     for i in range(4):
         angle = int(90 * i)
-        print(thresh, angle)
-        rot = TF.rotate(img=thresh, angle=angle)
-        img = np.where(np.int8(rot) == -1, 1, 0)
-        plt.imshow(img)
-        plt.show()
+        rot = rotate(thresh, angle)
+        img = cv.normalize(
+            src=rot, 
+            dst=None, 
+            alpha=0, 
+            beta=255, 
+            norm_type=cv.NORM_MINMAX, 
+            dtype=cv.CV_8U
+        )
         img_name = f_split[0] + "_" + str(angle) + f_split[1]
-        cv.imwrite("input/captcha_de-noised/"+img_name, img)
+        imsave("input/captcha_de-noised/"+img_name, img)
     
 
 def main():
