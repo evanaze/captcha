597c2e4a82f65509d55ab229b3a7ee161cd85bd8
Evan Azevedo <evan@azevedo.com>
2020-07-13 16:15:09 -0700

    update

--- src/config.py
+++ src/config.py
@@ -4,8 +4,8 @@ TRAIN_DATA="data/train_temp.csv"
 VALID_DATA="data/valid_temp.csv"
 TEST_DATA="data/test.csv"
 
-N_FOLDS=3
-TEST_SIZE=0.1
+N_FOLDS=2
+TEST_SIZE=0.2
 TRAIN_BATCH_SIZE=10
 VALID_BATCH_SIZE=25
 RAND_STATE=1212
